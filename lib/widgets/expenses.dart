e7e856f763d302b3ae252fd06b31d19b6aeb0e67
DenysLobur <denismachtalles@gmail.com>
2023-08-28 11:42:43 +0300

    initial

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expenses_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Eating in restaurant',
+        amount: 10.90,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Watching movie',
+        amount: 15.50,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('chart...'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
