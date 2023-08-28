5b6604fcb674e94d62256e57c718bf78979cc977
DenysLobur <denismachtalles@gmail.com>
2023-08-28 12:55:14 +0300

    Add TextField for Expense title (12)

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expenses_tracker/models/expense.dart';
+import 'package:expenses_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -28,7 +29,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('data data data'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
